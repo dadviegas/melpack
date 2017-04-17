0be80761a35a6d22b43dc725cd30306c1b48001e
dadviegas <david.viegas@talkdesk.com>
2017-04-17 14:51:18 +0100

    test location resolve on modules

--- packages/melpack-jasmine/index.js
+++ packages/melpack-jasmine/index.js
@@ -1,5 +1,5 @@
 require('babel-register')({
-  'presets': ['es2015', 'stage-2'],
+  'presets': ['es2015', 'stage-2'].map(require.resolve)
 })
 
 module.exports = require('./src/index.js').default
--- packages/melpack/index.js
+++ packages/melpack/index.js
@@ -2,7 +2,7 @@ require('babel-register')({
   presets: [
     'babel-preset-es2015',
     'babel-preset-stage-2',
-  ]
+  ].map(require.resolve)
 })
 
 module.exports = require('./src/index.js').default
